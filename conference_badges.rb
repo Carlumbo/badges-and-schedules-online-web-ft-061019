d8f52d1437ed7898e726f3922f1984f60889331f
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-18 06:03:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,8 +12,7 @@ def batch_badge_creator(attendees)
 end 
 
 def assign_rooms (attendees)
-  room_number = attendees.each_with_index
-  attendees.each_with_index do |badges, room_number|
+    attendees.each_with_index do |badges, room_number|
     "Hello, #{badges}! You'll be assigned to room #{room_number}!"
     end 
 end 
