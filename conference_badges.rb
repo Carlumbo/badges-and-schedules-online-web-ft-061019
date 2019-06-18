fd9846865ffd87b5bed7ca9630dcae11f2b50b4c
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-18 05:46:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
- attendees = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', and 'Matz']
+ attendees = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
  
  def badge_maker(name)
    puts "Hello, my name is #{name}"
